4028333697e51a85c92b0abb8308c2d06052b7c4
Darius Nagy <nagydarius97@gmail.com>
2020-10-02 21:55:40 +0300

    Added response status to controller advice so openAPI can add the response status to the endpoints

--- src/main/java/org/finance/mybank/exception/handlers/ExceptionHandlerController.java
+++ src/main/java/org/finance/mybank/exception/handlers/ExceptionHandlerController.java
@@ -6,15 +6,17 @@ import org.finance.mybank.exception.NotEnoughBalanceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static org.finance.mybank.exception.handlers.ApiResponseMessage.apiResponseMessage;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
 	@ExceptionHandler({InvalidDateException.class, InvalidQueryParamException.class, NotEnoughBalanceException.class})
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	protected ResponseEntity<?> handleBadRequest(RuntimeException e) {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 				.contentType(MediaType.APPLICATION_JSON)
